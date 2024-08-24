77a1355be07644ffefa187d497448e3590253b63
yejiin cho <87606729+bodhitrii@users.noreply.github.com>
2024-08-24 18:07:31 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__PAW_CONFIG__ = {
-    googleApiKey: 'AIzaSyBoX_m8IQZO2Fq__4XgkIRADhVXTJwtsbs',
-    googleSheetDocId: '1AKmXQRHbW2Cu0VpVzM5wewL5W8HtQRgTwhdAugdqJJU',
+    googleApiKey: 'AIzaSyA4YhWJI__XYBZp0NRCXa5L5A44aYIw_jU',
+    googleSheetDocId: '1M-Aq0DzwDv6J9024W2L8NY3rCsR0Bz3ni_EIT1_kvLU',
 }
